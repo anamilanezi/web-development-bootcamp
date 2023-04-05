51226ba6eaabf8889a45a7ba1c916c15cb77d647
Ana Milanezi <anamilanezi@gmail.com>
2023-04-05 18:17:09 -0300

    add javascript projects

--- javascript/dicee-challenge/index.js
+++ javascript/dicee-challenge/index.js
@@ -7,26 +7,28 @@ function changeImg(randomNumber) {
     return "images/dice" + randomNumber + ".png"
 }
 
-function play() {
+function throwDice(n) {
+    var randomNumber = randomDice()
+    var player = document.querySelector(".img"+ n)
+    player.src = changeImg(randomNumber)
+    return randomNumber
+}
 
-    var randomNumber1 = randomDice()
-    var player1 = document.querySelector(".img1")
-    player1.src = changeImg(randomNumber1)
+function gameplay() {
 
-    var randomNumber2 = randomDice()
-    var player2 = document.querySelector(".img2")
-    player2.src = changeImg(randomNumber2) 
-    
-    if (randomNumber1 > randomNumber2) {
+    var player1 = throwDice(1);
+    var player2 = throwDice(2);
+       
+    if (player1 > player2) {
         document.querySelector("h1").textContent = "Player 1 Wins!"
-    } else if (randomNumber1 == randomNumber2) {
+    } else if (player1 == player2) {
         document.querySelector("h1").textContent = "Draw!"
     } else {
         document.querySelector("h1").textContent = "Player 2 Wins!"
     }
-
 }
 
-var throwDice = document.querySelector("button")
-throwDice.addEventListener("click", play);
+
+var play = document.querySelector("button")
+play.addEventListener("click", gameplay);
 
